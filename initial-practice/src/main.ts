a9c5fdbe9c3060ac91d7b92a19173f3b018d0b35
Axxxxxel11 <128259347+Axxxxxel11@users.noreply.github.com>
2025-05-04 04:16:41 -0600

    feat/ cambios para swagger y cors

--- initial-practice/src/main.ts
+++ initial-practice/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Habilitar CORS para todas las rutas
+  app.enableCors({
+    origin: '*', // Permite todas las origenes
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Games API')
+    .setDescription('API para gestionar juegos')
+    .setVersion('1.0')
+    .addTag('games')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
